cee0b205789aa43eb323d8057cef680ef52616cc
Adi-Levy <adilevy15@gmail.com>
2020-06-14 19:14:04 +0300

    added base for implementation of drawable subclasses

--- apple.h
+++ apple.h
@@ -4,7 +4,41 @@
 
 #include "drawable.h"
 
+class Apple : public Drawable {
+public:
 
+	/**
+	 * @brief Initiate an apple at x,y
+	 */
+	Apple(unsigned short x, unsigned short y);
+
+	/**
+	 * @brief Move the object in the direction
+	 */
+	virtual void move(direction_t direction);
+
+	/**
+	 * @brief Draw the object
+	 */
+	virtual void draw();
+
+	/**
+	 * @brief Return an ID that is unique to
+	 * the dynamid type of the drawable object.
+	 */
+	virtual int id();
+
+	/**
+	 * @brief Is called whenever any refresh is required
+	 */
+	virtual void refresh();
+
+	/**
+	 * @brief Do a step in the 'game of life'
+	 * @param lst A list of all drawable objects in the world
+	 */
+	virtual void step(DrawableList& lst);
+};
 
 
 #endif 
--- monster.cpp
+++ monster.cpp
@@ -1,2 +1,26 @@
 #include "monster.h"
 
+
+Monster::Monster(unsigned short x, unsigned short y, int direction_hold) {
+
+}
+
+void Monster::move(direction_t direction) {
+
+}
+
+void Monster::draw() {
+
+}
+
+int Monster::id() {
+
+}
+
+void Monster::refresh() {
+
+}
+
+void Monster::step(DrawableList& lst) {
+
+}
